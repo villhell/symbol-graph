55980c6a70de1228062047710c6a74aa6c0e55cf
villhell <lilly.of.the.valley.to@gmail.com>
2023-06-30 21:32:50 +0900

    mod

--- src/SymbolGraph.tsx
+++ src/SymbolGraph.tsx
@@ -1,7 +1,13 @@
+const btnRead_Click = (e: any) => {
+  alert(e.target.value);
+};
 export const SymbolGraph = () => {
   return (
     <div>
       <h1>Symbol Graph</h1>
+      <label>address:</label>
+      <input type="text" id="address" name="address" required />
+      <input type="button" value="Read" onClick={btnRead_Click} />
     </div>
   );
 };
